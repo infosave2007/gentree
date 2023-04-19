f3e6a459aea08b4f10fcbf3c2ae0b385967dee19
infosave2007 <50740197+infosave2007@users.noreply.github.com>
2023-04-19 16:19:19 +0300

    Update Gentree.php

--- src/Gentree.php
+++ src/Gentree.php
@@ -2,39 +2,62 @@
 
 namespace Gentree;
 
-class TreeNode
+class Gentree
 {
-    private array $data;
-    private array $children = [];
+    private array $csvData;
 
-    public function __construct(array $data)
+    public function __construct(array $csvData)
     {
-        $this->data = $data;
+        $this->csvData = $csvData;
     }
 
-    public function getData(): array
+    public function buildTree(string $parentId = ''): array
     {
-        return $this->data;
-    }
-
-    public function addChild(TreeNode $child): void
-    {
-        $this->children[] = $child;
-    }
-
-    public function getChildren(): array
-    {
-        return $this->children;
-    }
-
-    public function toArray(): array
-    {
-        $result = $this->data;
-        if (!empty($this->children)) {
-            $result['Children'] = array_map(function ($child) {
-                return $child->toArray();
-            }, $this->children);
+        $branch = array();
+        foreach ($this->csvData as $row) {
+            if ($row['Parent'] === $parentId) {
+                $node = new TreeNode($row);
+                $children = $this->buildTree($row['Item Name']);
+                foreach ($children as $child) {
+                    $node->addChild($child);
+                }
+                $branch[] = $node;
+            }
         }
-        return $result;
+        return $branch;
+    }
+public function extendTree(array $tree): array
+    {
+        foreach ($tree as &$node) {
+            if ($node->getData()['Type'] === 'Прямые компоненты' && !empty($node->getData()['Relation'])) {
+                $relatedItem = null;
+                foreach ($this->csvData as $element) {
+                    if ($element['Item Name'] == $node->getData()['Relation']) {
+                        $relatedItem = $element;
+                        break;
+                    }
+                }
+                if ($relatedItem) {
+                    $relatedBranch = $this->buildTree($relatedItem['Item Name']);
+                    if ($relatedBranch) {
+                        $node->setChildren($this->extendTree($relatedBranch));
+                    }
+                }
+            } elseif (!empty($node->getChildren())) {
+                $node->setChildren($this->extendTree($node->getChildren()));
+            }
+        }
+
+        return $tree;
+    }
+
+    public function generateJsonTree(): string
+    {
+        $tree = $this->buildTree();
+        $extendedTree = $this->extendTree($tree);
+        $arrayTree = array_map(function ($node) {
+            return $node->toArray();
+        }, $extendedTree);
+        return json_encode($arrayTree, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
     }
 }
